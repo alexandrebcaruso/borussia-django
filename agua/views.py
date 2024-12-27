eab57f86da9645e602f1836e23d7cec750f9adff
A Caruso <alequis@gmail.com>
2024-12-27 15:30:41 -0300

    edit profile feature

--- agua/views.py
+++ agua/views.py
@@ -99,8 +99,9 @@ def profile(request):
 
     if request.method == 'POST':
         user = request.user
-        user.first_name = request.POST.get('first_name')
-        user.last_name = request.POST.get('last_name')
+        # Update the user's first name and last name
+        user.first_name = request.POST.get('first_name', user.first_name)
+        user.last_name = request.POST.get('last_name', user.last_name)
         user.save()
         messages.success(request, 'Your profile has been updated.')
         return redirect('profile')
